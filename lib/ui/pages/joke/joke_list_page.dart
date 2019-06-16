18fdfab2e4967526d40421028930eff1068a8a7e
nonybright <nonybrighto@gmail.com>
2019-06-16 12:26:28 +0100

    show movie details action

--- lib/ui/pages/joke/joke_list_page.dart
+++ lib/ui/pages/joke/joke_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tv_series_jokes/models/movie/movie.dart';
+import 'package:tv_series_jokes/navigation/router.dart';
 import 'package:tv_series_jokes/ui/widgets/app_drawer.dart';
 import 'package:tv_series_jokes/ui/widgets/joke/joke_add_button.dart';
 import 'package:tv_series_jokes/ui/widgets/joke/joke_list.dart';
@@ -19,7 +20,11 @@ class _JokeListPageState extends State<JokeListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.pageTitle),),
+      appBar: AppBar(title: Text(widget.pageTitle), actions: <Widget>[
+        if(widget.movie != null)IconButton(icon: Icon(Icons.info), onPressed: (){
+          gotoMovieDetialsPage(context, movie:widget.movie);
+        },)
+      ],),
       drawer: AppDrawer(),
       body: JokeList(),
       floatingActionButton: JokeAddButton(selectedMovie: widget.movie),
